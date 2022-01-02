8ad9e2364f6b1d5c1b7e3fc2647a60d302f579f8
JonCabot <jfc0053@gmail.com>
2022-01-02 15:44:26 -0600

    commit tree ed4e388f09e1416374b7c2fc4bca4c75dcee3390

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
   //add static methods here
-}
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-']+/g, "");
+  }
+
+  static titleize(string) {
+    if(!string) {
+        return '';
+      }
+      let arr = [];
+      let exludeWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+      arr = string.split(' ');
+      return arr.map((word, i) =>
+      {
+        return exludeWords.includes(word) && i!=0 ? [word] : word.charAt(0).toUpperCase() + word.slice(1);
+
+      }).join(' ');
+    }
+}  
